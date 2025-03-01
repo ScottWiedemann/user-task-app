090ff611cf69e52c1b6ff0cc08f28a12f9d2dcd5
Scott Wiedemann <scott.wiedemann.14@gmail.com>
2025-02-28 17:31:15 -0700

    Feat: Add simple search functionality
    Added a search using filtering of the task list

--- packages/web-app/src/app/tasks/tasks.service.ts
+++ packages/web-app/src/app/tasks/tasks.service.ts
@@ -43,11 +43,9 @@ export class TasksService {
 
   searchTask(search: string): void {
     if (search) {
-      // TODO: filter tasks which title include search value
-      throw new Error('Not implemented');
+      this.tasks = this.tasks.filter((task) => task.title.includes(search));
     } else {
-      // TODO: reload all tasks from storage
-      throw new Error('Not implemented');
+      this.getTasksFromStorage();
     }
   }
 }
